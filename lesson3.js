2196c7ae4f680e7a2a1ebf0438f2c131822ec169
faihundin <faihundin@mail.ru>
2019-10-02 10:23:55 +0300

    test

--- lesson3.js
+++ lesson3.js
@@ -30,7 +30,7 @@ function castingServerResponse(inputData) {
 function countBasketPrice(cartArr) {
     let cartCost = 0;
     for (let i = 0; i <= cartArr.length - 1; i++) {
-        cartCost += (+cartArr[i][2]*cartArr[i][1]); // [i][2] - цена, [i][1] - количество товаров
+        cartCost += (+cartArr[i][2] * cartArr[i][1]); // [i][2] - цена, [i][1] - количество товаров
     }
     return cartCost;
 }
@@ -46,14 +46,15 @@ while (confirm("Добавить еще один товар?")) {
     addGoodsInCart(cartServerResponse);
 }
 
-document.getElementById('price').innerHTML = `Цена всех товаров в корзине: ${countBasketPrice(cartArr)} $`; 
+document.getElementById('price').innerHTML = `Цена всех товаров в корзине: ${countBasketPrice(cartArr)} $`;
 
 /*
  * 4.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
  * for(…){// здесь пусто}
 */
 
-for(let i=1;i<=9;console.log(i++)){}
+for (let i = 1; i <= 9; console.log(i++)) {
+}
 
 /*
  * 5. *Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
@@ -64,7 +65,7 @@ for(let i=1;i<=9;console.log(i++)){}
  *  xxxxx
  */
 let pyramid = 'x';
-for (let i=1;i<=20;i++){
+for (let i = 1; i <= 20; i++) {
     console.log(`${pyramid}`);
-    pyramid+='x';
+    pyramid += 'x';
 }
